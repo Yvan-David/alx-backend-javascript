c5ea7224e784ebdd2dc9e102041a01ac0d4b93d4
Yvan-David <irankundayvan2020@gmail.com>
2024-01-05 15:18:05 +0200

    correcting errors

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -6,14 +6,17 @@ export default class HolbertonCourse{
     }
     get name(){
         if(typeof(this._name) === String) return(this._name)
+        else throw
     }
     set name(value){ if(typeof(this._name) === String) this._name = value}
     get length(){
         if(typeof(this._length) === Number) return(this._length)
+        else throw
     }
     set length(value){ if(typeof(this._length) === Number) this._length = value}
     get students(){
         if(typeof(this._students) === Array) return(this._students)
+        else throw
     }
     set students(value){ if(typeof(this._students) === Array) this._students = value}
 }
