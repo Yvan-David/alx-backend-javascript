c224ceb49b9d72f8b1d9db45abcb6820a9e2a799
Yvan-David <irankundayvan2020@gmail.com>
2024-01-08 14:05:50 +0200

    error correction eslint

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (const idx of array) {
-    const value = array[idx];
-    array[idx] = appendString + value;
+    let value = array[idx];
+    value = appendString + value;
   }
 
   return array;
